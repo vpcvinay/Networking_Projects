8e50229d358dda76e28b3ffc138bb48896785766
Purna Chandra Vinay Kumar. Vemali <46797670+vpcvinay@users.noreply.github.com>
2020-07-13 18:07:40 -0500

    Uploaded Tested files

---
+++ Network_Architecture_simulation/run.sh
@@ -0,0 +1,6 @@
+python node_program.py 0 40 3 "Message from node 0 to node 3 is Hello node 3, How are you:" 25 1 5 &
+python node_program.py 1 40 0 "Message from 1 to 0:" 25 0 2 &
+python node_program.py 2 40 2 1 3 &
+python node_program.py 3 40 3 2 4 &
+python node_program.py 4 40 4 3 5 &
+python node_program.py 5 40 0 "Message from 5 to 0:" 25 0 4&
